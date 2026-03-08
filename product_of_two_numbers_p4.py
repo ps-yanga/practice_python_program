9711828986f6a73b539f30f5d069a839f1cdc9f0
ps-yanga <yangaprincess03@gmail.com>
2026-03-08 22:50:17 +0800

    multipication

--- product_of_two_numbers_p4.py
+++ product_of_two_numbers_p4.py
@@ -1,6 +1,7 @@
+# multiplication
 # number 1
 num1 = int(input("Enter a number: "))
 # number 2
 num2 = int(input("Enter a number: "))
 # product
-print("THE PRODUCT OF TWO NUMEBER IS", num1 * num2)
+print("THE PRODUCT OF TWO NUMBERS IS", num1 * num2)
